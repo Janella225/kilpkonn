7fd02244e23378dc6cef322ac7684a7632c2966f
Janella225 <133005244+Janella225@users.noreply.github.com>
2023-05-22 11:09:53 +0300

    Add files via upload

---
+++ kilpkonnake.py
@@ -0,0 +1,13 @@
+import turtle
+
+
+turtle.bgcolor('yellow')
+turtle.pensize(3)
+turtle.color("lime")
+for i in range(18):
+    
+    turtle.forward(200)
+    turtle.right(100)
+    
+    
+turtle.exitonclick()
